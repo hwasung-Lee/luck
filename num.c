cd076003c58d2ad9f94ed1161f4163b10728d2d7
Hwasung Lee <hwasung@protonmail.ch>
2022-11-10 16:25:40 +0900

    Create num.c

---
+++ num.c
@@ -0,0 +1,24 @@
+#include <ncurses.c>
+
+//aligator
+void one()
+{
+}
+void two();
+void three();
+void four()
+{      
+        printw("          :::\n");
+        printw("        :+:  \n");
+        printw("       +:+ +:+ \n");
+        printw("     +#+  +:+  \n");
+        printw("   +#+#+#+#+#+\n");
+        printw("        #+#   \n");
+        printw("       ###    \n");
+
+}
+void five();
+void six();
+void seven();
+void eight();
+void nine();
